8d8ec716487d39e0ccbf2840694f4788c68ea5dc
Eddie <eherrerag.eh@gmail.com>
2019-02-17 12:27:15 -0700

    Added the Log statements for the life cycles and the permissions that the app is going to depend on

--- IntroToAndroidDev/app/src/main/java/com/googledevelopersgroup/intro/MainActivity.java
+++ IntroToAndroidDev/app/src/main/java/com/googledevelopersgroup/intro/MainActivity.java
@@ -2,27 +2,53 @@ package com.googledevelopersgroup.intro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final String LIFECYCLE_TAG = "LifeCycle";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LIFECYCLE_TAG, "onCreate() called -Activity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     @Override
+    protected void onResume() {
+        Log.d(LIFECYCLE_TAG, "onResume() called -Activity");
+        super.onResume();
+    }
+
+    @Override
     protected void onStop() {
+        Log.d(LIFECYCLE_TAG, "onStop() called -Activity");
         super.onStop();
+
     }
 
     @Override
     protected void onDestroy() {
+        Log.d(LIFECYCLE_TAG, "onDestroy() called -Activity");
         super.onDestroy();
     }
 
     @Override
+    protected void onRestart() {
+        Log.d(LIFECYCLE_TAG, "onRestart() called -Activity");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(LIFECYCLE_TAG, "onPause() called -Activity");
+        super.onPause();
+
+    }
+
+    @Override
     protected void onStart() {
+        Log.d(LIFECYCLE_TAG, "onStart() called -Activity");
         super.onStart();
+
     }
 }
