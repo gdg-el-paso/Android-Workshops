21746d57ec2b757e20d87510d3b8bec252de1706
Eddie <eherrerag.eh@gmail.com>
2019-02-17 13:31:15 -0700

    Added the FlashFrag which is going to contain the flashlight interface and the ViewPager to the MainActivity. Iam also adding the lifecycles for the fragment

--- IntroToAndroidDev/app/src/main/java/com/googledevelopersgroup/intro/MainActivity.java
+++ IntroToAndroidDev/app/src/main/java/com/googledevelopersgroup/intro/MainActivity.java
@@ -1,16 +1,24 @@
 package com.googledevelopersgroup.intro;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    //The tag we use to filter the logcat, for life-cycles
     private final String LIFECYCLE_TAG = "LifeCycle";
+    //The request id we are going to use for the camera permission
+    private final int CAM_REQUEST = 123;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(LIFECYCLE_TAG, "onCreate() called -Activity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.ViewPager);
     }
 
     @Override
@@ -42,13 +50,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         Log.d(LIFECYCLE_TAG, "onPause() called -Activity");
         super.onPause();
-
     }
 
     @Override
     protected void onStart() {
         Log.d(LIFECYCLE_TAG, "onStart() called -Activity");
         super.onStart();
-
     }
 }
