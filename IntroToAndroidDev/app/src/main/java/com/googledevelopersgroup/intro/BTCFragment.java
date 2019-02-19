17b57ef7536742f0d2009c3f5c65c68291fb8b12
Eddie <eherrerag.eh@gmail.com>
2019-02-19 14:09:52 -0700

    The request for the BTC is working, I am getting the price of BTC

--- IntroToAndroidDev/app/src/main/java/com/googledevelopersgroup/intro/BTCFragment.java
+++ IntroToAndroidDev/app/src/main/java/com/googledevelopersgroup/intro/BTCFragment.java
@@ -1,6 +1,10 @@
 package com.googledevelopersgroup.intro;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,11 +20,14 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpResponse;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.InputStream;
 
 /**
  * This fragment is going to display the price of the Btc
@@ -51,37 +58,32 @@ public class BTCFragment extends Fragment {
         btcPrice = view.findViewById(R.id.btcPrice);
         btcBtn = view.findViewById(R.id.btcBtn);
         btcBtn.setOnClickListener(v -> {
-            Log.i(TAG,"Getting the price");
+            Log.i(TAG, "Getting the price");
+
             //we are going going to request the price here
-            JsonObjectRequest btcRequest = new JsonObjectRequest
-                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            String price;
-                            Log.i(TAG,"Response!");
-                            try {
-                                price = response.getString("rate");
-                                btcPrice.setText(price);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
+            @SuppressLint("SetTextI18n") JsonObjectRequest btcRequest = new JsonObjectRequest
+                    (Request.Method.GET, url, null, response -> { ;
+                        Log.i(TAG, "Response!");
+                        try {
+                            JSONObject usdPrice = (response.getJSONObject("bpi")).getJSONObject("USD");
 
+                            btcPrice.setText("$"+String.valueOf(usdPrice.getString("rate")));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                    }, new Response.ErrorListener() {
 
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            // TODO: Handle error
-                            Log.i(TAG,"Error");
-                            try {
-                                throw new Exception(error);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
+                    }, error -> {
+                        // TODO: Handle error
+                        Log.i(TAG, "Error");
+                        try {
+                            throw new Exception(error);
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
                     });
             queue.add(btcRequest);
         });
         super.onViewCreated(view, savedInstanceState);
     }
+
 }
